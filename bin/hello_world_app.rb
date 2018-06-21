d8b19fe62c435a4e755d529b119feb6641bf716a
Adam Stulpa <adam.stulpa@gooddata.com>
2018-06-21 14:37:40 +0200

    Get hello brick parameters from environment

--- bin/hello_world_app.rb
+++ bin/hello_world_app.rb
@@ -3,6 +3,7 @@
 require_relative '../lib/gooddata'
 include GoodData::Bricks
 
-params = {}
+params_json = ENV['BRICK_PARAMS_JSON']
+params = params_json == nil ? {} : JSON.parse(params_json)
 @brick_result = GoodData::Bricks::Pipeline.hello_world_brick_pipeline.call(params)
 pp @brick_result
